e2e51c28a5d82e42723606ff1035aabe715060b6
lionelgo <29477918+lionelgo@users.noreply.github.com>
2023-12-01 15:42:32 +0100

    let's assume this is deployment v0
    Signed-off-by: lionelgo <29477918+lionelgo@users.noreply.github.com>

--- charts/oai-5g-ran/oran-local/deploycn.bash
+++ charts/oai-5g-ran/oran-local/deploycn.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p /tmp/pcap
-cd ../../oai-5g-core/oai-5g-basic && helm dependency update && helm spray .
+cd ../../oai-5g-core/oai-5g-basic && helm dependency update && helm install oai-5g-basic .
--- charts/oai-5g-ran/oran-local/endcn.bash
+++ charts/oai-5g-ran/oran-local/endcn.bash
@@ -1,8 +1,2 @@
 #!/bin/bash
-helm uninstall oai-udr
-helm uninstall oai-udm 
-helm uninstall oai-spgwu-tiny 
-helm uninstall oai-smf 
-helm uninstall oai-nrf 
-helm uninstall oai-ausf 
-helm uninstall oai-amf
+helm uninstall oai-5g-basic
